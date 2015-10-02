abcdfa7f88475911a72a18811fb5f61d561f3de2
tayazhurba <taisiya_zhurba@mail.ru>
2015-10-03 02:12:22 +0800

    go on

--- src/main/scala/funsets/FunSets.scala
+++ src/main/scala/funsets/FunSets.scala
@@ -2,71 +2,42 @@ package funsets
 
 import common._
 
-/**
- * 2. Чисто функциональные множества.
- */
+
 object FunSets {
-  /**
-   * Мы представляем множество их характеристической функцией. Например, его предикатом
-   * `contains`.
-   */
+
   type Set = Int => Boolean
 
-  /**
-   * Проверяет, содержит ли множество данный элемент.
-   */
   def contains(s: Set, elem: Int): Boolean = s(elem)
 
-  /**
-   * Возвращает множество из одного переданного элемента.
-   */
-  def singletonSet(elem: Int): Set = Set(elem)
+  def singletonSet(elem: Int): Set = {(num:Int)=> num == elem}
 
-  /**
-   * Возвращает объединение двух данных множеств,
-   * множества всех элементов , входящих либо в `s`, либо в `t`.
-   */
-  def union(s: Set, t: Set): Set = i => s(i)|| t(i)
+  def union(s: Set, t: Set): Set = {(num:Int)=>s(num)||t(num)}
 
-  /**
-   * Возвращает пересечение двух заданных множеств,
-   * множество всех элементов, которые входят и в `s` и в `t`.
-   */
-  def intersect(s: Set, t: Set): Set = ???
+  def intersect(s: Set, t: Set): Set = {(num:Int)=>s(num)&&t(num)}
 
-  /**
-   * Возвращает разницу между двумя данными множествами,
-   * множество всех элементов, входящих в `s` и не входящих в `t`.
-   */
-  def diff(s: Set, t: Set): Set = ???
+  def diff(s: Set, t: Set): Set = {(num:Int)=>s(num)&& !t(num)}
 
-  /**
-   * Возвращает подмножество `s` для которых `p` выполняется.
-   */
-  def filter(s: Set, p: Int => Boolean): Set = ???
+  def filter(s: Set, p: Int => Boolean): Set = {(num:Int)=>s(num)&&p(num)}
 
-  /**
-   * Ограничения для `forall` и `exists` равны +/- 1000.
-   */
   val bound = 1000
 
-  /**
-   * Проверяет, все ли числа множества `s` из заданного интервалла удовлетворяют `p`.
-   */
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a < -1000) true
+      else if (s(a) && !p(a)) false
+      else iter(a-1)
     }
-    iter(???)
+    iter(bound)
   }
 
-  /**
-   * Проверяет, существует ли число в интервалле из множества `s`
-   * которое удовлетворяет `p`.
-   */
-  def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = {
+    def iter(a: Int): Boolean = {
+      if (a < -1000) false
+      else if (s(a) && p(a)) true
+      else iter(a-1)
+    }
+    iter(bound)
+  }
 
   /**
    * Возвращает множество преобразованное применением `f` к каждому элементу `s`.
--- src/test/scala/funsets/FunSetSuite.scala
+++ src/test/scala/funsets/FunSetSuite.scala
@@ -5,107 +5,110 @@ import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
-/**
- * Этот класс - набор методов для объектов в классе FunSets. Для запуска тестов вы можете:
-  *  - набрать команду "test" в sbt консоли (sbt test)
- *   - В IDE правой кнопкой на файле "Run As" - "JUnit Test"
- */
+
 @RunWith(classOf[JUnitRunner])
 class FunSetSuite extends FunSuite {
 
-
-  /**
-   * Ссылка на scaladoc - очень понятный и детализированный туториал по FunSuite
-   *
-   * http://doc.scalatest.org/1.9.1/index.html#org.scalatest.FunSuite
-   *
-   * Операторы
-   *  - test
-   *  - ignore
-   *  - pending
-   */
-
-  /**
-   * Тесты написаны с использованием оператора "test" и метода "assert".
-   */
   test("string take") {
     val message = "hello, world"
     assert(message.take(5) == "hello")
   }
 
-  /**
-   * Для тестов ScalaTest, существуют специальный оператор эквивалентности "===" который
-   * может быть использован внутри "assert". Если assertion не выполняется, оба значения будут напечатаны в сообщении об ошибке.
-   * В противном случае, когда используем "==", сообщение о провале теста скажет только
-   * "assertion failed", без показа значений.
-   *
-   * Попробуйте это в действии! Измените значения так, чтобы assert давал fail и
-   * взгляните на сообщение об ошибке.
-   */
   test("adding ints") {
     assert(1 + 2 === 3)
   }
 
-  
   import FunSets._
 
   test("contains implemented") {
-    assert(contains(x => true, 100))
+    assert(contains(x => true, 7))
   }
-  
-  /**
-   * Когда пишите тесты, вам бы часто хотелось повторно использовать определенные значения
-   * для множества тестов. Например, Мы бы хотели создать Int-set и сделать для него
-   * несколько тестов
-   * 
-   * 
-   * Вместо того чтобы копи-пастить код для создания тестового set-а в каждом тесте
-   * мы можем сохранить его в классе теста используя val:
-   * 
-   *   val s1 = singletonSet(1)
-   * 
-   * Однако, что случится, если метод "singletonSet" содержит баг и падает? Тогда
-   * тестовые методы даже не будут запущены, т.к. создание тест-класса падает!
-   * 
-   * Вместо этого мы, мы поместим совместно разделяемые значения в отдельный trait 
-   * (трейты похожи на абстрактные классы), и создадим экземпляр внутри каждого тестового метода.
-   * 
-   */
 
   trait TestSets {
     val s1 = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
+    val s4 = singletonSet(4)
   }
 
-  /**
-   * Этот тест сейчас выключен (засчет использования "ignore") поскольку метод
-   * "singletonSet" еще не реализован и тест провалится.
-   * 
-   * Когда вы напишите реализацию "singletonSet", замените 
-   * функцию "ignore" на "test".
-   */
-  ignore("singletonSet(1) contains 1") {
-    
-    /**
-     * Мы создадим новый экземпляр трейта "TestSets", это даст нам доступ
-     * к значениям от "s1" до "s3". 
-     */
+  test("singletonSet contains") {
     new TestSets {
-      /**
-       * Строковый аргумент команды "assert" - это сообщение, которое выводится в случае
-       * провала теста. Это помогает идентифицировать, какой assert провалился.
-       */
       assert(contains(s1, 1), "Singleton")
+      assert(!contains(s2, 1), "Singleton")
     }
   }
 
-  ignore("union contains all elements") {
+  test("union, intersect, diff") {
     new TestSets {
-      val s = union(s1, s2)
-      assert(contains(s, 1), "Union 1")
-      assert(contains(s, 2), "Union 2")
-      assert(!contains(s, 3), "Union 3")
+      val u = union(s2, s3)
+      val uu = union(u,s4)
+      val i = intersect(u, s2)
+      val d = diff(uu, s2)
+      assert(contains(u, 3))
+      assert(contains(i, 2))
+      assert(!contains(i, 4))
+      assert(contains(d, 3))
+      assert(contains(d, 4))
+      assert(!contains(d, 2))
+      assert(!contains(uu, 1))
+    }
+  }
+
+  test("forall") {
+    new TestSets {
+      val u = union(s2, s3)
+      val uu = union(u,s4)
+      val i = intersect(u, s2)
+      val d = diff(uu, s2)
+      val ff = (a:Int) => a>2
+      assert(!forall(u, ff))
+      assert(!forall(i, ff))
+      assert(forall(d, ff))
+      assert(!forall(uu, ff))
+    }
+  }
+
+  test("filter") {
+    new TestSets {
+      val u = union(s2, s3)
+      val uu = union(u,s4)
+      val i = intersect(u, s2)
+      val d = diff(uu, s2)
+      val ff = (a:Int) => a<3
+      val f1 = filter(u, ff)
+      val f2 = filter(uu, ff)
+      val f3 = filter(i, ff)
+      val f4 = filter(d, ff)
+      assert(contains(f1, 2))
+      assert(!contains(f1, 3))
+      assert(contains(f2, 2))
+      assert(!contains(f2, 4))
+      assert(!contains(f2, 1))
+      assert(contains(f3, 2))
+      assert(!contains(f3,4))
+      assert(!contains(f4,1))
+      assert(!contains(f4,2))
+      assert(!contains(f4,3))
+      assert(!contains(f4,4))
+    }
+  }
+
+  test("exists") {
+    new TestSets {
+      val u = union(s2, s3)
+      val uu = union(u,s4)
+      val i = intersect(u, s2)
+      val d = diff(uu, s2)
+      val ff = (a:Int) => a<3
+      val ff1 = (a:Int) => a>3
+      assert(exists(u, ff))
+      assert(!exists(u, ff1))
+      assert(exists(uu, ff))
+      assert(exists(uu, ff1))
+      assert(exists(i, ff))
+      assert(!exists(i, ff1))
+      assert(!exists(d,ff))
+      assert(exists(d,ff1))
     }
   }
 }
